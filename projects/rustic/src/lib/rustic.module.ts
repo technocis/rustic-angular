b2175098f1bb35fedb4260c741e8ad83d45f894e
Rohith Balaji <me@therohith.com>
2021-08-16 17:11:36 +0530

    List Components

--- projects/rustic/src/lib/rustic.module.ts
+++ projects/rustic/src/lib/rustic.module.ts
@@ -3,6 +3,8 @@ import { RusticComponent } from './rustic.component';
 import { ButtonComponent } from './button/button.component';
 import { CheckboxComponent } from './checkbox/checkbox.component';
 import { SpinnerComponent } from './spinner/spinner.component';
+import { ListItemComponent } from './list-item/list-item.component';
+import { ListComponent } from './list/list.component';
 
 @NgModule({
   declarations: [
@@ -10,6 +12,8 @@ import { SpinnerComponent } from './spinner/spinner.component';
     ButtonComponent,
     CheckboxComponent,
     SpinnerComponent,
+    ListItemComponent,
+    ListComponent,
   ],
   imports: [],
   exports: [
