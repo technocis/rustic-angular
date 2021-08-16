55a01def889ba98c2aa92ab98f85fc3a8414e7c6
Rohith Balaji <me@therohith.com>
2021-08-16 19:15:52 +0530

    Migrate to MDX

--- projects/rustic/src/lib/button/button.component.ts
+++ projects/rustic/src/lib/button/button.component.ts
@@ -1,15 +1,13 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'cis-button',
   templateUrl: './button.component.html',
   styleUrls: ['./button.component.css'],
 })
-export class ButtonComponent implements OnInit {
+export class ButtonComponent {
   @Input() type: 'primary' | 'secondary' | 'warning' = 'primary';
   @Input() size: 'small' | 'medium' | 'large' = 'medium';
 
   constructor() {}
-
-  ngOnInit(): void {}
 }
--- projects/rustic/src/lib/checkbox/checkbox.component.ts
+++ projects/rustic/src/lib/checkbox/checkbox.component.ts
@@ -1,14 +1,12 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'cis-checkbox',
   templateUrl: './checkbox.component.html',
   styleUrls: ['./checkbox.component.css'],
 })
-export class CheckboxComponent implements OnInit {
+export class CheckboxComponent {
   @Input() label: string = '';
 
   constructor() {}
-
-  ngOnInit(): void {}
 }
